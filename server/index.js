2d28aac02052ae6b03b30a088ea7c76db934624d
Anthony Diké <anthonydike@nyu.edu>
2020-05-31 11:17:36 -0400

    chore: installed nodemon

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+// incoming REQuest, outcoming RESponse
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello Meower 😺❤️',
+  });
+});
+
 app.listen(5001, () => {
   console.log('Listening on http://localhost:5001');
 });
